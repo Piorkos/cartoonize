614df46d3299fa417a58eeaece110c915c6a9f69
Dominik Piórkowski <dominik.piorkowski@wp.pl>
2019-05-20 19:38:42 +0200

    Dodanie wyboru źródła obrazu (kamera lub plik)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::string;
+
+    cout << "Select option:" << "\n";
+    cout << "1 - to capture image from camer" << "\n";
+    cout << "2 - to read image from file" << "\n";
+    char mode{'1'};
+    cin >> mode;
+
+    if(mode == '1')
+    {
+
+    }
+    else if(mode == '2')
+    {
+        cout << "Provide path to image file." << "\n";
+        cin.ignore(32767, '\n');
+        string path{" "};
+        std::getline(cin, path);
+
+    }
+
     return 0;
 }
