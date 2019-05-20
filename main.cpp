888cf2c463c60c75acbdb6f105ebb99db7903a44
Dominik Piórkowski <dominik.piorkowski@wp.pl>
2019-05-20 19:53:09 +0200

    Przechwytywanie obrazu z kamery.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
 
+#include "opencv2/core.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+
 int main()
 {
+    using cv::Mat;
+
     using std::cout;
     using std::cin;
+    using std::cerr;
     using std::string;
 
     cout << "Select option:" << "\n";
@@ -15,7 +22,34 @@ int main()
 
     if(mode == '1')
     {
+        Mat frame;
+//        --- INITIALIZE VIDEOCAPTURE
+        cv::VideoCapture cap;
+        int deviceID = 0;             // 0 = open default camera
+        int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+        cap.open(deviceID + apiID);
+        if (!cap.isOpened()) {
+            cerr << "ERROR! Unable to open camera\n";
+            return -1;
+        }
+//        cap.set(cv::CAP_PROP_FRAME_WIDTH, 400);
+//        cap.set(cv::CAP_PROP_FRAME_HEIGHT, 300);
 
+//        --- GRAB AND WRITE LOOP
+        cout << "Start grabbing" << "\n"
+             << "Press any key to terminate" << "\n";
+        for (;;)
+        {
+            cap.read(frame);
+            if (frame.empty()) {
+                cerr << "ERROR! blank frame grabbed\n";
+                break;
+            }
+//            show live and wait for a key with timeout long enough to show images
+            imshow("Live", frame);
+            if (cv::waitKey(5) >= 0)
+                break;
+        }
     }
     else if(mode == '2')
     {
